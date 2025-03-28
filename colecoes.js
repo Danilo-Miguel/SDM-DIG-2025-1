00cbbf7b48365c94cb1d59f0062858cb77548324
Erick Patrick <erickckpatrick17@gmail.com>
2025-03-28 19:59:36 -0300

    ..

---
+++ colecoes.js
@@ -0,0 +1,44 @@
+//let numeros = [];
+
+//let produtos = ["nootebock", "celular", "camera"];
+
+//const usuarios = [
+// {
+//  nome: "eu",
+// idade: "15",
+//email: "wdad@gmail.com",
+//},
+//];
+
+//console.log(produtos[1]);
+
+// Arrays
+
+// let numeros = [10, 20 ,30 ,40, 50]
+
+// console.log(numeros[0])
+// console.log(numeros[1])
+// console.log(numeros[2])
+// console.log(numeros[3])
+// console.log(numeros[4])
+
+let frutas = ["Cajá", "Umbu", "Maça", "Tangerina"];
+//console.log(frutas);
+
+//frutas.push("Uva"); // Adiciona ao final da lista ["Cajá", "Umbu", "Maça", "Tangerina" "Uva"]
+//console.log(frutas);
+
+//frutas.unshift("Banana"); // Adiciona ao inicio da lista ["Banana", "Cajá", "Umbu", "Maça", "Tangerina" "Uva"]
+//console.log(frutas);
+
+//frutas.pop(); // Remove o ultimo elemento da lista
+//console.log(frutas);
+
+//frutas.shift(); // Remove o primeiro elemento da lista
+//console.log(frutas);
+
+//console.log([0]);
+
+for (let i = 0; i < frutas.length; i++) {
+  console.log(`Indice ${i}: ${frutas[i]}`);
+}
---
+++ variaveis.js
@@ -0,0 +1,25 @@
+//variavel declarada
+//var nome_usuario = ",,,,,,,,,,,,";
+
+//nome_usuario = "eu";
+//console.log(nome_usuario);
+
+//const idade = 9;
+//console.log(idade);
+
+//variavel inicializada
+//let idade = 39;
+//let naturalidade = "Sao paulo";
+//console.log(naturalidade);
+//const PI = 3.14;
+
+// String para valores númericos
+let num = Number("42");
+let float = parseFloat("5,5");
+let inteiro = parseInt("10");
+
+let num1 = 2;
+let num2 = 2;
+let soma = num1 + num2;
+
+console.log(soma);
